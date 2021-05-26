e1aae5641d787c05e5f72d6508059b241d9f7e42
unknown <quantran2381@gmail.com>
2021-05-26 14:45:42 +0700

    training4

---
+++ Training4/src/Task1/JokeAPI.js
@@ -0,0 +1,78 @@
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+
+// optimize time
+function useDebounce(text, delay) {
+  delay = delay || 500;
+
+  const [debounced, setDebounced] = useState(text);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounced(text);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [text, delay]);
+
+  return debounced;
+}
+
+export default function JokeAPI() {
+  // state
+  const [joke, setJoke] = useState([]);
+
+  const [showJoke, setShowJoke] = useState(false);
+
+  const debounceVal = showJoke;
+  
+  //   call api
+  //   cors problem
+  const headers = {
+    "Content-Type": "text/plain",
+  };
+
+  useEffect(async () => {
+    console.log("use effect");
+    if (!showJoke) {
+      console.log("not showjoke");
+
+      return;
+    } else {
+      await axios
+
+        .get("https://official-joke-api.appspot.com/jokes/random", { headers })
+        .then((res) => {
+          console.log("setJoke");
+
+          setJoke(res.data);
+
+          setShowJoke(false);
+        });
+    }
+  }, [debounceVal]);
+
+  console.log("showJoke", showJoke);
+  return (
+    <Fragment>
+      <h2> {joke.setup ? joke.setup : <Fragment />}</h2>
+      <div className="joke">
+        <p className="textarea" style={{ color: "tomato" }}>
+          {" "}
+          {joke.punchline ? joke.punchline : "\nJoke In here"} 😎
+        </p>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setShowJoke(true);
+          }}
+        >
+          Click here to joke
+        </button>
+      </div>
+    </Fragment>
+  );
+}
---
+++ Training4/src/Task2/CountriesAPI.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React, { Fragment, useEffect, useState } from "react";
+
+// optimize time
+function useDebounce(text, delay) {
+  delay = delay || 500;
+
+  const [debounced, setDebounced] = useState(text);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounced(text);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [text, delay]);
+
+  return debounced;
+}
+
+export default function CountriesAPI() {
+  // cors
+  const headers = {
+    "Content-Type": "text/plain",
+  };
+
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const debouncedVal = useDebounce(searchTerm);
+
+  console.log(debouncedVal);
+
+  useEffect(async () => {
+    await axios
+
+      .get("https://restcountries.eu/rest/v2/all", { headers })
+      .then((res) => {
+        console.log(res.data);
+
+        setCountries(res.data);
+        setSearch(res.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (searchTerm === "") {
+      setSearch(countries);
+    } else if (debouncedVal) {
+      const searchResult = countries.filter((item) => {
+        return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+
+      setSearch(searchResult);
+    }
+  }, [debouncedVal]);
+
+  const renderCountry = () => {
+    return search?.map((country, index) => {
+      return (
+        <div className="col-3">
+          <div className="card mb-3">
+            <img src={country.flag} alt="Avatar" style={{ width: "100%" }} />
+            <div className="container">
+              <h4 style={{ marginTop: "10px" }}>
+                <b>{country.name}</b>
+              </h4>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <Fragment>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="text"
+            className="searchTerm"
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+            placeholder="What are you looking for?"
+          />
+          <button type="submit" className="searchButton">
+            <i className="fa fa-search" />
+          </button>
+        </div>
+      </div>
+      <div className="row">{renderCountry()}</div>;
+    </Fragment>
+  );
+}
